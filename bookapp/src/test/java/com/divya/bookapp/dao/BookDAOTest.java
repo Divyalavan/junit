c85302b939abcea30dca40f18eb41627c9047b29
Administrator <Administrator@DBMS-PC>
2017-02-22 15:11:11 +0530

    junit

---
+++ bookapp/src/test/java/com/divya/bookapp/dao/BookDAOTest.java
@@ -0,0 +1,36 @@
+package com.divya.bookapp.dao;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import com.divya.bookapp.model.Book;
+
+public class BookDAOTest {
+	
+	@Test
+	public void testSave() {
+		
+	
+	Book book=new Book();
+	book.setName("java");
+	book.setPrice(500f);
+	BookDAO dao=new BookDAO();
+	dao.Save(book);
+	}
+	
+
+	@Test
+	public void testFindAll() {
+		BookDAO dao=new BookDAO();
+		List<Book> list=dao.findAll();
+		for(Book b:list)
+		{
+		System.out.println(b);
+		
+	}
+
+}
+}
